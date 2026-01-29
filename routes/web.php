a4153898ebba02b1cabe526141500208640d55a6
amine <aminear220@gmail.com>
2026-01-29 11:32:23 +0100

    vfvf

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\DashController;
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\ContactController;
+
+
+
+
+
+Route::get('/', [DashController::class, 'dashboard']);
+
+ 
+
+// Route::get('/contact ', [ContactController::class ,'Contact']);
+
+Route::get('/groups', [GroupController::class , 'page']);
+Route::post('/groups', [GroupController::class , 'create']);
+Route::get('/groups', [GroupController::class , 'read']);
+
+Route::get('/contacts', [ContactController::class, 'index']);
+Route::post('/contacts', [ContactController::class, 'store']);
+Route::post('contacts/{contact}', [CachingIterator::class , 'delete']);
