d4da0684a8e422cdbf0575ae4950b27a4e39ab21
amine <aminear220@gmail.com>
2026-02-02 09:10:07 +0100

    add  delete function

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,17 @@ Route::get('/', [DashController::class, 'dashboard']);
 
 // Route::get('/contact ', [ContactController::class ,'Contact']);
 
-Route::get('/groups', [GroupController::class , 'page']);
-Route::post('/groups', [GroupController::class , 'create']);
-Route::get('/groups', [GroupController::class , 'read']);
+// Route::get('/groups', [GroupController::class , 'page']);
+// Route::post('/groups', [GroupController::class , 'create']);
+// Route::get('/groups', [GroupController::class , 'read']);
+
+
+Route::get('/groups', [GroupController::class, 'index']);
+Route::post('/groups', [GroupController::class, 'create']);
+
+Route::get('/groups/{group}/edit', [GroupController::class, 'edit']);
+Route::put('/groups/{group}', [GroupController::class, 'update']);
+Route::delete('/groups/{group}', [GroupController::class, 'destroy']);
 
 Route::get('/contacts', [ContactController::class, 'index']);
 Route::post('/contacts', [ContactController::class, 'store']);
